1c86e660576a6a647821afa12a90cf0b54ad50ba
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-03-20 11:28:10 -0300

    code refactor

--- otp-generator/app.js
+++ otp-generator/app.js
@@ -1,57 +1,47 @@
 const btnCode = document.getElementById("btn-code");
 const showCode = document.getElementById("show-code");
-
 const inputs = document.querySelectorAll("input");
 const checkCodeBtn = document.getElementById("check-code");
 
-const checkCode = [];
-const fullCode = [];
+btnCode.addEventListener("click", generateCode);
 
-btnCode.addEventListener("click", () => {
-  for (let i = 1; i < 7; i++) {
-    let code = Math.floor(Math.random() * 9);
-    fullCode.push(code.toString());
-  }
+checkCodeBtn.addEventListener("click", verifyCode);
 
-  let textCode = fullCode.join(" ");
-  showCode.innerText = textCode;
-});
+function generateCode() {
+  const codeLength = 6;
+  // Array.from crea un nuevo array en base a un objeto iterable
+  const code = Array.from({ length: codeLength }, () =>
+    Math.floor(Math.random() * 10)
+  ).join(" ");
+  showCode.innerText = code;
+}
 
 function inputValues() {
+  // expresion regular para que solo se validen los numeros del 0 al 9
   const regex = /[^0-9]/g;
 
+  // se obtiene el value de cada input
   inputs.forEach((input, index) => {
     input.addEventListener("input", () => {
-      const inputValue = input.value.replace(regex);
+      const inputValue = input.value.replace(regex, "");
       if (inputValue && index < inputs.length - 1) {
         inputs[index + 1].focus();
       }
-      checkCodeFunction(inputValue);
     });
   });
 }
 
-function checkCodeFunction(value) {
-  checkCodeBtn.addEventListener("click", () => {
-    checkCode.push(value);
-    if (arraysAreEqual(checkCode, fullCode)) {
-      console.log("pasa");
-    } else {
-      console.log("el code no coincide");
-    }
-  });
-}
+function verifyCode() {
+  const userInput = Array.from(inputs, (input) => input.value).join(" ");
+  const code = showCode.innerText;
 
-function arraysAreEqual(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
+  if (userInput === code) {
+    document.body.style.backgroundColor = "green";
+    checkCodeBtn.innerText = "accepted code";
+  } else {
+    document.body.style.backgroundColor = "red";
+    checkCodeBtn.innerText = "denied code";
   }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
 }
 
 inputValues();
